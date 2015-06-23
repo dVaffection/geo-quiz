b1f6693dcfbbda7edd99257553955be07a1ede7f
dV <dVaffection@gmail.com>
2015-06-22 21:35:16 -0700

    Added questions and visuals

--- app/src/main/java/com/dvlab/geoquiz/QuizActivity.java
+++ app/src/main/java/com/dvlab/geoquiz/QuizActivity.java
@@ -6,6 +6,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -13,6 +15,18 @@ public class QuizActivity extends ActionBarActivity {
 
     private Button trueButton;
     private Button falseButton;
+    private ImageButton nextButton;
+    private ImageButton prevButton;
+    private TextView questionTextView;
+
+    private TrueFalse[] questionBank = new TrueFalse[]{
+            new TrueFalse(R.string.question_oceans, true),
+            new TrueFalse(R.string.question_africa, false),
+            new TrueFalse(R.string.question_americas, true),
+            new TrueFalse(R.string.question_asia, false),
+            new TrueFalse(R.string.question_mideast, true),
+    };
+    private int currentQuestionIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +37,8 @@ public class QuizActivity extends ActionBarActivity {
         trueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                int messageResource = isCorrectAnswer(true);
+                Toast.makeText(QuizActivity.this, messageResource, Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -31,11 +46,74 @@ public class QuizActivity extends ActionBarActivity {
         falseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                int messageResource = isCorrectAnswer(false);
+                Toast.makeText(QuizActivity.this, messageResource, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        questionTextView = (TextView) findViewById(R.id.question_text_view);
+        int question = questionBank[currentQuestionIndex].getQuestion();
+        questionTextView.setText(question);
+        questionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToTheNextQuestion();
+            }
+        });
+
+        nextButton = (ImageButton) findViewById(R.id.next_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToTheNextQuestion();
+            }
+        });
+
+        prevButton = (ImageButton) findViewById(R.id.prev_button);
+        prevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToThePrevQuestion();
             }
         });
     }
 
+    private void moveToTheNextQuestion() {
+        currentQuestionIndex++;
+        if (currentQuestionIndex >= questionBank.length) {
+            currentQuestionIndex = 0;
+        }
+
+        displayQuestion(currentQuestionIndex);
+    }
+
+    private void moveToThePrevQuestion() {
+        currentQuestionIndex--;
+        if (currentQuestionIndex < 0) {
+            currentQuestionIndex = questionBank.length - 1;
+        }
+
+        displayQuestion(currentQuestionIndex);
+    }
+
+    private void displayQuestion(int index) {
+        int question = questionBank[index].getQuestion();
+        questionTextView.setText(question);
+    }
+
+    private int isCorrectAnswer(boolean userChoice) {
+        boolean questionChoice = questionBank[currentQuestionIndex].isTrueQuestion();
+
+        int messageResource;
+        if ((userChoice && questionChoice) || (!userChoice && !questionChoice)) {
+            messageResource = R.string.correct_toast;
+            moveToTheNextQuestion();
+        } else {
+            messageResource = R.string.incorrect_toast;
+        }
+
+        return messageResource;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
